00b39c67df6e8851234e154a2153a0d9af4b8329
gk <govindrajkendre@gmail.com>
2026-05-24 10:07:55 +0530

    First commit

---
+++ src/com/demo/git/Test1.java
@@ -0,0 +1,8 @@
+package com.demo.git;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.out.println("Test");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module GitDemoprojectV1 {
+}
